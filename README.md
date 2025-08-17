7bc096985f2bba66f6479dae7b21b9cd65126c9d
GregTash <gregtash4@gmail.com>
2025-08-17 16:07:44 +1000

    Add LSP configuration.

---
+++ README.md
@@ -0,0 +1,2 @@
+1. Install latest version of Neovim
+2. Install nerdfonts and configure them for terminal and monospace font
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,6 @@
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set autoindent")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
