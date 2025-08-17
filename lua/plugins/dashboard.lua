a8e9d63148f07d93a351c2035ecd59addb05941c
GregTash <gregtash4@gmail.com>
2025-08-17 16:53:42 +1000

    Change dashboard.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,10 +1,43 @@
 return {
-	"nvimdev/dashboard-nvim",
-	event = "VimEnter",
-	config = function()
-		require("dashboard").setup({
-			-- config
-		})
-	end,
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+    "goolord/alpha-nvim",
+    event = "VimEnter",  -- Load on startup
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        -- Set header
+        dashboard.section.header.val = {
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+        }
+
+        -- Set menu buttons (removed Quit and Find file)
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+            dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+        }
+
+        -- Optional footer (requires your fork with fortune)
+        -- local fortune = require("alpha.fortune")
+        -- dashboard.section.footer.val = fortune()
+
+        -- Apply config
+        alpha.setup(dashboard.opts)
+
+        -- Disable folding in alpha buffer
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "alpha",
+            callback = function()
+                vim.opt_local.foldenable = false
+            end,
+        })
+    end,
 }
