8a9a89d459c5ab3c0534d9af847acbe2cd3776ca
GregTash <gregtash4@gmail.com>
2025-08-18 11:29:01 +1000

    Add ability to read errors.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -21,12 +21,36 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({})
-			lspconfig.clangd.setup({})
+			-- Setup servers
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			-- LSP keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+
+			-- 🔑 Diagnostics keymaps
+			vim.keymap.set("n", "<leader>e", function()
+				vim.diagnostic.open_float(nil, { focus = false, border = nil })
+			end, { desc = "Show error under cursor" })
+
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics list" })
+
+			-- Optional: show diagnostics inline
+			vim.diagnostic.config({
+				virtual_text = false, -- set to true if you want inline error messages
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
 		end,
-	},
+	}
 }
